caface036324a55af517cfc58c14a38486f23255
NichlasMau <Nich5384@stud.kea.dk>
2023-03-13 14:23:36 +0100

    fixed Irepository

--- src/main/java/com/example/superhelte_v4_springboot_jdbc/repositories/Irepository.java
+++ src/main/java/com/example/superhelte_v4_springboot_jdbc/repositories/Irepository.java
@@ -4,29 +4,28 @@ import com.example.superhelte_v4_springboot_jdbc.dto.City_superhero;
 import com.example.superhelte_v4_springboot_jdbc.dto.SuperheroPowerByName;
 import com.example.superhelte_v4_springboot_jdbc.model.Superhero;
 import com.example.superhelte_v4_springboot_jdbc.model.SuperheroPowerCount;
-import com.example.superhelte_v4_springboot_jdbc.services.SuperheroService;
 
 import java.util.List;
 
 public interface Irepository {
 
-    default Superhero getSuperHero(String name){
+    default Superhero getSuperhero(String name){
         return null;
     }
 
-    default List<Superhero> getSuperheroes() {
+    default List<Superhero> getAllSuperheros() {
         return null;
     }
 
-    default List<SuperheroPowerCount> getSuperheroPowerCount(){
+    default List<SuperheroPowerCount> getSuperheroPower(String name){
         return null;
     }
 
-    default List<SuperheroPowerByName> getSuperpowerByName(){
+    default List<SuperheroPowerByName> getSuperPowerByName(String name){
         return null;
     }
 
-    default List<City_superhero> getCitySuperhero(){
+    default List<City_superhero> getCitySuperhero(String name){
         return null;
     }
 
